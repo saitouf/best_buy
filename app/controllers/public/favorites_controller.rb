7470e1074778ee191d962999e4383e3d55ce0415
EC2 Default User <ec2-user@ip-172-31-18-16.ap-northeast-1.compute.internal>
2023-05-11 04:38:12 +0000

    [Add]いいね機能非同期

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,13 +4,11 @@ class Public::FavoritesController < ApplicationController
     @post_item = PostItem.find(params[:post_item_id])
     favorite = current_customer.favorites.new(post_item_id: @post_item.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     @post_item = PostItem.find(params[:post_item_id])
     favorite = current_customer.favorites.find_by(post_item_id: @post_item.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
