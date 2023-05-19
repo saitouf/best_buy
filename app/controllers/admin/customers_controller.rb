cdea66788bc29ad303148856bc27640967390751
EC2 Default User <ec2-user@ip-172-31-18-16.ap-northeast-1.compute.internal>
2023-05-19 12:27:31 +0000

    [Add]管理側投稿機能、レイアウト調整

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,30 +1,30 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
     @customers = Customer.all
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
     @post_comment = PostComment.new
     @post_items = @customer.post_items
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-      redirect_to customer_path(@customer), notice: "You have updated admin successfully."
+      redirect_to admin_customers_path, notice: "You have updated admin successfully."
     else
       render "edit"
     end
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:name, :email, :is_deleted)
   end
--- app/controllers/admin/post_items_controller.rb
+++ app/controllers/admin/post_items_controller.rb
@@ -1,18 +1,18 @@
 class Admin::PostItemsController < ApplicationController
-  
+
   def index
     @post_items = PostItem.all
   end
-  
+
   def show
     @post_comment = PostComment.new
     @post_item = PostItem.find(params[:id])
   end
-  
+
   def edit
     @post_item = PostItem.find(params[:id])
   end
-  
+
   def update
     @post_item = PostItem.find(params[:id])
     if @post_item.update(post_item_params)
@@ -21,13 +21,13 @@ class Admin::PostItemsController < ApplicationController
       render "edit"
     end
   end
-  
+
   def destroy
     @post_item = PostItem.find(params[:id])
     @post_item.destroy
     redirect_to admin_post_items_path
   end
-  
+
   private
 
   def post_item_params
