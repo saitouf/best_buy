8ff0078e9099f6f22e01ebf60c60501f2a5d4c56
EC2 Default User <ec2-user@ip-172-31-18-16.ap-northeast-1.compute.internal>
2023-05-28 10:27:47 +0000

    [Add]グループ機能修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "ws://13.114.157.161/cable"
+  config.action_cable.allowed_request_origins = ['http://13.114.157.161']
 end
