9a1ed61631df4abc1c986b92c9fc594cd6f9a62f
EC2 Default User <ec2-user@ip-172-31-18-16.ap-northeast-1.compute.internal>
2023-05-29 09:27:32 +0000

    [Add]機能修正

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -2,7 +2,9 @@ class Admin::PostCommentsController < ApplicationController
   before_action :authenticate_admin!
   
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @post_item = PostItem.find(params[:post_item_id])
+    @post_comment=PostComment.find_by(id: params[:id], post_item_id: params[:post_item_id])
+    @post_comment.destroy
+    @post_comment = PostComment.new
   end
 end
--- app/controllers/admin/post_items_controller.rb
+++ app/controllers/admin/post_items_controller.rb
@@ -1,6 +1,6 @@
 class Admin::PostItemsController < ApplicationController
   before_action :authenticate_admin!
-  
+
    # 投稿、新しい・古い・コメント数多い・いいね数多い順に並び替え
   def index
     if params[:latest]
@@ -43,9 +43,9 @@ class Admin::PostItemsController < ApplicationController
   def destroy
     @post_item = PostItem.find(params[:id])
     @post_item.destroy
-    redirect_to admin_post_items_path, notice: "投稿を削除しました。"
+    redirect_to admin_customer_path(@post_item.customer), notice: "投稿を削除しました。"
   end
-  
+
   def search
     @post_items = PostItem.search(params[:keyword]).page(params[:page]).per(10)
     @keyword = params[:keyword]
